50113e0c5041851ed3c49429ff2c6b23ec3af358
thành kaka <thanhkakacsb@gmail.com>
2023-05-19 21:05:03 +0700

    rename id DatMon to oder; class an to hide

--- KFC.js
+++ KFC.js
@@ -2,13 +2,12 @@ function header(){
     window.addEventListener('scroll', cuon);
     function cuon(){
         const y = window.pageYOffset;// đọc vị trí của trang
-        const show= document.getElementById("DatMon");
+        const show= document.getElementById("oder");
         if(y<100){
-            show.classList.add(`an`);
+            show.classList.add(`hide`);
         }
-        
         if (y>=100){
-            show.classList.remove(`an`);
+            show.classList.remove(`hide`);
         }
     }
 }
