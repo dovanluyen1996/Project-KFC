23ae0606ce606c343514a2dac1896c5cd61259b7
thành kaka <thanhkakacsb@gmail.com>
2023-05-29 18:52:00 +0700

    check lại toàn bộ projeck nhé

--- KFC.js
+++ KFC.js
@@ -25,3 +25,68 @@ function footer(){
     }); 
 }
 footer();
+function Accordion1(){
+    let i=0;
+    var a = document.getElementById(`stick1`);
+    var b = document.getElementById(`text1`);
+    a.addEventListener(`click`, down1);
+    function down1(){
+        i=i+1;
+        console.log(i);
+        if (i%2==0){
+            b.classList.add(`hide`)
+            if(i!==0){
+                a.classList.remove(`stick`)
+            }
+        }
+        else{
+            b.classList.remove(`hide`)
+            a.classList.add(`stick`)
+        }
+    }
+}
+Accordion1();
+
+function Accordion2(){
+    let i=0;
+    var a = document.getElementById(`stick2`);
+    var b = document.getElementById(`text2`);
+    a.addEventListener(`click`, down1);
+    function down1(){
+        i=i+1;
+        console.log(i);
+        if (i%2==0){
+            b.classList.add(`hide`)
+            if(i!==0){
+                a.classList.remove(`stick`)
+            }
+        }
+        else{
+            b.classList.remove(`hide`)
+            a.classList.add(`stick`)
+        }
+    }
+}
+Accordion2();
+
+function Accordion3(){
+    let i=0;
+    var a = document.getElementById(`stick3`);
+    var b = document.getElementById(`text3`);
+    a.addEventListener(`click`, down1);
+    function down1(){
+        i=i+1;
+        console.log(i);
+        if (i%2==0){
+            b.classList.add(`hide`)
+            if(i!==0){
+                a.classList.remove(`stick`)
+            }
+        }
+        else{
+            b.classList.remove(`hide`)
+            a.classList.add(`stick`)
+        }
+    }
+}
+Accordion3();
